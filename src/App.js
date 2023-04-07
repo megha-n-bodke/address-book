06f8ce85fcc84866eae71418f6036ba4d1026dfe
Megha Bodke <megha.n.bodke@gmail.com>
2023-04-07 12:30:12 +0530

    Accept and add contact to local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
       <h3>Home Page</h3>
+      <HomePage></HomePage>
     </>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+
+const HomePage = () => {
+  const [formDataArray, setFormDataArray] = useState([]);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+  });
+
+  useEffect(() => {
+    const storedFormDataArray = localStorage.getItem("formDataArray");
+    if (storedFormDataArray) {
+      setFormDataArray(JSON.parse(storedFormDataArray));
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormDataArray((prevFormDataArray) => [...prevFormDataArray, formData]);
+    localStorage.setItem(
+      "formDataArray",
+      JSON.stringify([...formDataArray, formData])
+    );
+  };
+  return (
+    <section>
+      <div className="container">
+        <div className="row">
+          <h3>Create Contact</h3>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                type="text"
+                className="mb-3"
+                placeholder="Enter Your Name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="number"
+                placeholder="Enter Your Mobile Number"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <button type="submit">Create Contact</button>
+              <button>Cancel</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
